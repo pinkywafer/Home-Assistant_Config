7aa757855747c9fa0b9141e54d6898610683c38a
pinkywafer <pinkywafer@gmail.com>
2020-01-01 15:43:08 +0000

    update garbage_collection

--- custom_components/garbage_collection/sensor.py
+++ custom_components/garbage_collection/sensor.py
@@ -1,7 +1,9 @@
 """Sensor platform for garbage_collection."""
 from homeassistant.helpers.entity import Entity
 import homeassistant.util.dt as dt_util
+import holidays
 import logging
+import locale
 from datetime import datetime, date, timedelta
 from homeassistant.core import HomeAssistant, State
 from typing import List, Any
@@ -38,6 +40,7 @@ from .const import (
     CONF_DATE,
     CONF_EXCLUDE_DATES,
     CONF_INCLUDE_DATES,
+    CONF_MOVE_COUNTRY_HOLIDAYS,
     CONF_PERIOD,
     CONF_FIRST_WEEK,
     CONF_SENSORS,
@@ -128,6 +131,18 @@ class GarbageCollection(Entity):
         )
         self.__include_dates = to_dates(config.get(CONF_INCLUDE_DATES, []))
         self.__exclude_dates = to_dates(config.get(CONF_EXCLUDE_DATES, []))
+        country_holidays = config.get(CONF_MOVE_COUNTRY_HOLIDAYS)
+        self.__holidays = []
+        if country_holidays is not None and country_holidays != "":
+            this_year = dt_util.now().date().year
+            years = [this_year, this_year + 1]
+            try:
+                for date, name in holidays.CountryHoliday(
+                    country_holidays, years=years
+                ).items():
+                    self.__holidays.append(date)
+            except KeyError:
+                _LOGGER.error("Invalid country code (%s)", country_holidays)
         self.__period = config.get(CONF_PERIOD)
         self.__first_week = config.get(CONF_FIRST_WEEK)
         self.__next_date = None
@@ -290,28 +305,31 @@ class GarbageCollection(Entity):
             _LOGGER.debug(f"({self.__name}) Unknown frequency {self.__frequency}")
             return None
 
+    def __insert_include_date(self, day1: date, next_date: date) -> date:
+        include_dates = list(filter(lambda date: date >= day1, self.__include_dates))
+        if len(include_dates) > 0 and include_dates[0] < next_date:
+            return include_dates[0]
+        else:
+            return next_date
+
+    def __skip_holiday(self, day: date) -> date:
+        return day + timedelta(days=1)
+
     def get_next_date(self, day1: date) -> date:
-        """Find the next date starting from day1.
-        Looks at include and exclude days"""
+        """Find the next date starting from day1."""
         first_day = day1
         i = 0
-        while True:
+        while i < 365:
             next_date = self.find_candidate_date(first_day)
-            include_dates = list(
-                filter(lambda date: date >= day1, self.__include_dates)
-            )
-            if len(include_dates) > 0 and include_dates[0] < next_date:
-                next_date = include_dates[0]
+            while next_date in self.__holidays:
+                next_date = self.__skip_holiday(next_date)
+            next_date = self.__insert_include_date(first_day, next_date)
             if next_date not in self.__exclude_dates:
-                break
-            else:
-                first_day = next_date + timedelta(days=1)
+                return next_date
+            first_day = next_date + timedelta(days=1)
             i += 1
-            if i > 365:
-                _LOGGER.error("(%s) Cannot find any suitable date", self.__name)
-                next_date = None
-                break
-        return next_date
+        _LOGGER.error("(%s) Cannot find any suitable date", self.__name)
+        return None
 
     async def async_update(self) -> None:
         """Get the latest data and updates the states."""
@@ -328,10 +346,9 @@ class GarbageCollection(Entity):
         if self.date_inside(today):
             next_date = self.get_next_date(today)
             if next_date is not None:
-                next_date_year = next_date.year
                 if not self.date_inside(next_date):
                     if self.__first_month <= self.__last_month:
-                        next_year = date(next_date_year + 1, self.__first_month, 1)
+                        next_year = date(year + 1, self.__first_month, 1)
                         next_date = self.get_next_date(next_year)
                         _LOGGER.debug(
                             "(%s) Did not find the date this year, "
@@ -339,7 +356,7 @@ class GarbageCollection(Entity):
                             self.__name,
                         )
                     else:
-                        next_year = date(next_date_year, self.__first_month, 1)
+                        next_year = date(year, self.__first_month, 1)
                         next_date = self.get_next_date(next_year)
                         _LOGGER.debug(
                             "(%s) Arrived to the end of date range, "
