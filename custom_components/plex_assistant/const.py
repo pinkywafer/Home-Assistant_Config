3ae5a7211f3be07b4ed6114eeeba4482226db820
pinkywafer <pinkywafer@gmail.com>
2022-08-12 19:03:49 +0100

    removes plex assistant

--- custom_components/plex_assistant/const.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-from homeassistant.const import __version__
-from awesomeversion import AwesomeVersion
-
-DOMAIN = "plex_assistant"
-HA_VER_SUPPORTED = AwesomeVersion(__version__) >= AwesomeVersion("2021.2.0")
-_LOGGER = logging.getLogger(__name__)
