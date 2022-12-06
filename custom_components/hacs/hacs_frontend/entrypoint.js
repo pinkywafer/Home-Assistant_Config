d16fc9a6416946fe0d90589c770df958b19e0277
pinkywafer <pinkywafer@gmail.com>
2022-12-06 19:19:53 +0000

    updates hacs to 1.27.2 ref #37

--- custom_components/hacs/hacs_frontend/entrypoint.js
+++ custom_components/hacs/hacs_frontend/entrypoint.js
@@ -1,9 +1,9 @@
 
 try {
-  new Function("import('/hacsfiles/frontend/main-7bc9a818.js')")();
+  new Function("import('/hacsfiles/frontend/main-ad130be7.js')")();
 } catch (err) {
   var el = document.createElement('script');
-  el.src = '/hacsfiles/frontend/main-7bc9a818.js';
+  el.src = '/hacsfiles/frontend/main-ad130be7.js';
   el.type = 'module';
   document.body.appendChild(el);
 }
