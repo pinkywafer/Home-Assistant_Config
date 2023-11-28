415d8938b78d87ca9080c94fa03fcb4c7bdd1f6e
pinkywafer <pinkywafer@gmail.com>
2023-11-28 18:47:10 +0000

    update various hacs lovelace components closes #37

--- www/community/vertical-stack-in-card/vertical-stack-in-card.js
+++ www/community/vertical-stack-in-card/vertical-stack-in-card.js
@@ -1,176 +1,189 @@
+console.log(`%cvertical-stack-in-card\n%cVersion: ${'0.4.4'}`, 'color: #1976d2; font-weight: bold;', '');
+
 class VerticalStackInCard extends HTMLElement {
-    constructor() {
-        super();
-        // Make use of shadowRoot to avoid conflicts when reusing
-        this.attachShadow({ mode: 'open' });
+  constructor() {
+    super();
+  }
+
+  setConfig(config) {
+    this._cardSize = {};
+    this._cardSize.promise = new Promise((resolve) => (this._cardSize.resolve = resolve));
+
+    if (!config || !config.cards || !Array.isArray(config.cards)) {
+      throw new Error('Card config incorrect');
     }
+    this._config = config;
+    this._refCards = [];
+    this.renderCard();
+  }
 
-    setConfig(config) {
-        if (!config || !config.cards || !Array.isArray(config.cards)) {
-            throw new Error('Card config incorrect');
-        }
-        
-        const shadow = this.shadowRoot;
-        while (shadow.hasChildNodes()) {
-            shadow.removeChild(shadow.lastChild);
-        }
-        
-        const card = document.createElement("ha-card");
-        const cardContent = document.createElement("div");
-        
-        card.header = config.title;
-        this._refCards = [];
-        
-        const _createThing = (tag, config) => {
-            const element = document.createElement(tag);
-            try {
-                element.setConfig(config);
-            } catch (err) {
-                console.error(tag, err);
-                return _createError(err.message, config);
-            }
-            return element;
-        };
-
-        const _createError = (error, config) => {
-            return _createThing("hui-error-card", {
-                type: "error",
-                error,
-                config,
-            });
-        };
-
-        const _fireEvent = (ev, detail, entity=null) => {
-            ev = new Event(ev, {
-                bubbles: true,
-                cancelable: false,
-                composed: true,
-            });
-            ev.detail = detail || {};
-
-            if (entity) {
-                entity.dispatchEvent(ev);
-            } else {
-                document
-                    .querySelector("home-assistant")
-                    .shadowRoot.querySelector("home-assistant-main")
-                    .shadowRoot.querySelector("app-drawer-layout partial-panel-resolver")
-                    .shadowRoot.querySelector("ha-panel-lovelace")
-                    .shadowRoot.querySelector("hui-root")
-                    .shadowRoot.querySelector("ha-app-layout #view")
-                    .firstElementChild
-                    .dispatchEvent(ev);
-            }
-        }
-
-        config.cards.forEach((item) => {
-            let tag = item.type;
-
-            if (tag.startsWith("divider")) {
-                tag = `hui-divider-row`;
-            } else if (tag.startsWith("custom:")) {
-                tag = tag.substr("custom:".length);
-            } else {
-                tag = `hui-${tag}-card`;
-            }
-
-            if (customElements.get(tag)) {
-                const element = _createThing(tag, item);
-                cardContent.appendChild(element);
-                this._refCards.push(element);
-            } else {
-                // If element doesn't exist (yet) create an error
-                const element = _createError(
-                    `Custom element doesn't exist: ${tag}.`,
-                    item
-                );
-                element.style.display = "None";
-
-                const time = setTimeout(() => {
-                    element.style.display = "";
-                }, 2000);
-
-                // Remove error if element is defined later
-                customElements.whenDefined(tag).then(() => {
-                    clearTimeout(time);
-                    _fireEvent("ll-rebuild", {}, element);
-                });
-
-                cardContent.appendChild(element);
-                this._refCards.push(element);
-            }
-        });
-        card.appendChild(cardContent);
-        shadow.appendChild(card);
+  async renderCard() {
+    const config = this._config;
+    if (window.loadCardHelpers) {
+      this.helpers = await window.loadCardHelpers();
     }
+    const promises = config.cards.map((config) => this.createCardElement(config));
+    this._refCards = await Promise.all(promises);
 
-    set hass(hass) {
-        if (this._refCards) {
-            this._refCards.forEach((card) => {
-                card.hass = hass;
-            });
-        }
+    // Style cards
+    this._refCards.forEach((card) => {
+      if (card.updateComplete) {
+        card.updateComplete.then(() => this.styleCard(card));
+      } else {
+        this.styleCard(card);
+      }
+    });
+
+    // Create the card
+    const card = document.createElement('ha-card');
+    const cardContent = document.createElement('div');
+    card.header = config.title;
+    card.style.overflow = 'hidden';
+    this._refCards.forEach((card) => cardContent.appendChild(card));
+    if (config.horizontal) {
+      cardContent.style.display = 'flex';
+      cardContent.childNodes.forEach((card) => {
+        card.style.flex = '1 1 0';
+        card.style.minWidth = 0;
+      });
     }
-
-    connectedCallback() {
-        this._refCards.forEach((element) => {
-            let fn = () => {
-                this._card(element);
-            };
-
-            if(element.updateComplete) {
-                element.updateComplete.then(fn);
-            } else {
-                fn();
-            }
-        });
+    card.appendChild(cardContent);
+    
+    const shadowRoot = this.shadowRoot || this.attachShadow({mode: 'open'});
+    while (shadowRoot.hasChildNodes()) {
+      shadowRoot.removeChild(shadowRoot.lastChild);
     }
+    shadowRoot.appendChild(card);
 
-    _card(element) {
-        if (element.shadowRoot) {
-            if (!element.shadowRoot.querySelector('ha-card')) {
-                let searchEles = element.shadowRoot.getElementById("root");
-                if (!searchEles) {
-                    searchEles = element.shadowRoot.getElementById("card");
-                }
-                if (!searchEles) return;
-                searchEles = searchEles.childNodes;
-                for (let i = 0; i < searchEles.length; i++) {
-                    if(searchEles[i].style !== undefined){
-                        searchEles[i].style.margin = "0px";
-                    }
-                    this._card(searchEles[i]);
-                }
-            } else {
-                let ele = element.shadowRoot.querySelector('ha-card')
-                ele.style.boxShadow = 'none';
-                ele.style.background = 'transparent';
-                ele.style.borderRadius = '0';
-            }
+    // Calculate card size
+    this._cardSize.resolve();
+  }
+
+  async createCardElement(cardConfig) {
+    const createError = (error, origConfig) => {
+      return createThing('hui-error-card', {
+        type: 'error',
+        error,
+        origConfig
+      });
+    };
+
+    const createThing = (tag, config) => {
+      if (this.helpers) {
+        if (config.type === 'divider') {
+          return this.helpers.createRowElement(config);
         } else {
-            if (typeof element.querySelector === 'function' && element.querySelector('ha-card')) {
-                let ele = element.querySelector('ha-card')
-                ele.style.boxShadow = 'none';
-                ele.style.background = 'transparent';
-                ele.style.borderRadius = '0';
-            }
-            let searchEles = element.childNodes;
-            for (let i = 0; i < searchEles.length; i++) {
-                if (searchEles[i] && searchEles[i].style) {
-                    searchEles[i].style.margin = "0px";
-                }
-                this._card(searchEles[i]);
-            }
+          return this.helpers.createCardElement(config);
         }
+      }
+
+      const element = document.createElement(tag);
+      try {
+        element.setConfig(config);
+      } catch (err) {
+        console.error(tag, err);
+        return createError(err.message, config);
+      }
+      return element;
+    };
+
+    let tag = cardConfig.type;
+    if (tag.startsWith('divider')) {
+      tag = `hui-divider-row`;
+    } else if (tag.startsWith('custom:')) {
+      tag = tag.substr('custom:'.length);
+    } else {
+      tag = `hui-${tag}-card`;
     }
 
-    getCardSize() {
-        let totalSize = 0;
-        this._refCards.forEach((element) => {
-            totalSize += typeof element.getCardSize === 'function' ? element.getCardSize() : 1;
+    const element = createThing(tag, cardConfig);
+    element.hass = this._hass;
+    element.addEventListener(
+      'll-rebuild',
+      (ev) => {
+        ev.stopPropagation();
+        this.createCardElement(cardConfig).then(() => {
+          this.renderCard();
         });
-        return totalSize;
+      },
+      { once: true }
+    );
+    return element;
+  }
+
+  set hass(hass) {
+    this._hass = hass;
+    if (this._refCards) {
+      this._refCards.forEach((card) => {
+        card.hass = hass;
+      });
     }
+  }
+
+  styleCard(element) {
+    const config = this._config;
+    if (element.shadowRoot) {
+      if (element.shadowRoot.querySelector('ha-card')) {
+        let ele = element.shadowRoot.querySelector('ha-card');
+        ele.style.boxShadow = 'none';
+        ele.style.borderRadius = '0';
+        ele.style.border = "none";
+        if ('styles' in config) {
+          Object.entries(config.styles).forEach(([key, value]) => ele.style.setProperty(key, value));
+        }
+      } else {
+        let searchEles = element.shadowRoot.getElementById('root');
+        if (!searchEles) {
+          searchEles = element.shadowRoot.getElementById('card');
+        }
+        if (!searchEles) return;
+        searchEles = searchEles.childNodes;
+        for (let i = 0; i < searchEles.length; i++) {
+          if (searchEles[i].style) {
+            searchEles[i].style.margin = '0px';
+          }
+          this.styleCard(searchEles[i]);
+        }
+      }
+    } else {
+      if (typeof element.querySelector === 'function' && element.querySelector('ha-card')) {
+        let ele = element.querySelector('ha-card');
+        ele.style.boxShadow = 'none';
+        ele.style.borderRadius = '0';
+        ele.style.border = "none";
+        if ('styles' in config) {
+          Object.entries(config.styles).forEach(([key, value]) => ele.style.setProperty(key, value));
+        }
+      }
+      let searchEles = element.childNodes;
+      for (let i = 0; i < searchEles.length; i++) {
+        if (searchEles[i] && searchEles[i].style) {
+          searchEles[i].style.margin = '0px';
+        }
+        this.styleCard(searchEles[i]);
+      }
+    }
+  }
+
+  _computeCardSize(card) {
+    if (typeof card.getCardSize === 'function') {
+      return card.getCardSize();
+    }
+    return customElements
+      .whenDefined(card.localName)
+      .then(() => this._computeCardSize(card))
+      .catch(() => 1);
+  }
+
+  async getCardSize() {
+    await this._cardSize.promise;
+    const sizes = await Promise.all(this._refCards.map(this._computeCardSize));
+    return sizes.reduce((a, b) => a + b, 0);
+  }
 }
 
 customElements.define('vertical-stack-in-card', VerticalStackInCard);
+
+window.customElements.get('vertical-stack-in-card').getConfigElement = function() {
+  return document.createElement('hui-stack-card-editor');
+}
